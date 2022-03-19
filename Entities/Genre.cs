653c0015abc30d4856bff9e14744026e4dac7015
Stoycho Kostov <stoichokostov@gmail.com>
2022-03-18 23:43:41 -0500

    adding custom attribute and model validations

--- Entities/Genre.cs
+++ Entities/Genre.cs
@@ -1,12 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using MoviesAPI.Validations;
 
 namespace MoviesAPI.Entities
 {
-    public class Genre
+    public class Genre : IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "The field with name {0} is required.")]
         [StringLength(10)]
+        //[FirstLetterUppercase]
         public string Name { get; set; }
 
         [Range(18,120)] 
@@ -16,5 +18,17 @@ namespace MoviesAPI.Entities
         [Url]
         public string Url { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                var firstLetter = Name[0].ToString();
+
+                if (firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("First letter should be uppercase", new string[] { nameof(Name) });
+                }
+            }
+        }
     }
 }
