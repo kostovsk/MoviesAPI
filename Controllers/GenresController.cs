79e69b1bc490edc51381824703912f03b7696ea4
Stoycho Kostov <stoichokostov@gmail.com>
2022-03-28 17:04:15 -0500

    cleanup

--- Controllers/GenresController.cs
+++ Controllers/GenresController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using MoviesAPI.Entities;
 using MoviesAPI.Filter;
-using MoviesAPI.Services;
 
 namespace MoviesAPI.Controllers
 {
@@ -12,63 +11,43 @@ namespace MoviesAPI.Controllers
     [ApiController]
     public class GenresController : ControllerBase
     {
-        private readonly IRepository _repository;
         private readonly ILogger<GenresController> _logger;
 
-        public GenresController(IRepository repository, ILogger<GenresController> logger)
+        public GenresController(ILogger<GenresController> logger)
         {
-            _repository = repository;
             _logger = logger;
         }
 
         [HttpGet] // api/genres
-        [HttpGet("list")] // api/genres/list
-        [HttpGet("/allgenres")] //allgenres
-        //[ResponseCache(Duration = 60)]
-        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        [ServiceFilter(typeof(MyActionFilter))]
         public async Task<ActionResult<List<Genre>>> Get()
         {
             _logger.LogInformation("Getting all the genres");
-            return await _repository.GetAllGenres();
+            return new List<Genre>() { new Genre() { Id = 1, Name = "Drama"}};
         }
 
         // api/genres/{id}
         [HttpGet("{id:int}", Name="getGenre")]
         public ActionResult<Genre> Get(int id)
         {
-            _logger.LogDebug("get by id method executing...");
-            var genre = _repository.GetGenreById(id);
-
-            if (genre == null)
-            {
-                _logger.LogWarning($"Genre with Id {id} not found.");
-                _logger.LogError("this is a log error");
-                //throw new ApplicationException();
-                return NotFound();
-            }
-
-            return genre;
+            throw new NotImplementedException();
         }
 
         [HttpPost]
         public ActionResult Post([FromBody] Genre genre)
         {
-            _repository.AddGenre(genre);
-
-            return NoContent();
+            throw new NotImplementedException();
         }
 
         [HttpPut]
         public ActionResult Put([FromBody] Genre genre)
         {
-            return NoContent();
+            throw new NotImplementedException();
         }
 
         [HttpDelete]
         public ActionResult Delete()
         {
-            return NoContent();
+            throw new NotImplementedException();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using MoviesAPI.Filter;
-using MoviesAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,49 +10,17 @@ builder.Services.AddControllers(options =>
 {
     options.Filters.Add(typeof(MyExceptionFilter));
 });
-builder.Services.AddResponseCaching();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
-builder.Services.AddSingleton<IRepository, InMemoryRepository>();
-builder.Services.AddTransient<MyActionFilter>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 using var loggerFactory = LoggerFactory.Create(loggingBuilder => loggingBuilder
-    .SetMinimumLevel(LogLevel.Trace)
     .AddConsole());
 
 var app = builder.Build();
 
 ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
-
-app.Use(async (context, next) =>
-{
-    using (var swapStream = new MemoryStream())
-    {
-        var originalResponseBody = context.Response.Body;
-        context.Response.Body = swapStream;
-
-        await next.Invoke();
-
-        swapStream.Seek(0, SeekOrigin.Begin);
-        string responseBody = new StreamReader(swapStream).ReadToEnd();
-        swapStream.Seek(0, SeekOrigin.Begin);
-
-        await swapStream.CopyToAsync(originalResponseBody);
-        context.Response.Body = originalResponseBody;
-
-        logger.LogInformation(responseBody);
-    }
-});
-
-app.Map("/map1", (app) =>
-{
-    app.Run(async context =>
-    {
-        await context.Response.WriteAsync("I am short-circuiting the pipeline");
-    });
-});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -65,8 +32,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseRouting();
-
-app.UseResponseCaching();
 
 app.UseAuthentication();
 
