52037aaa3316a27abf1c01691b1f78d9ec061f7c
Stoycho Kostov <stoichokostov@gmail.com>
2022-03-31 10:21:33 -0500

    custom display errors

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MoviesAPI;
+using MoviesAPI.APIBehavior;
 using MoviesAPI.Filter;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add(typeof(MyExceptionFilter));
-});
+    options.Filters.Add(typeof(ParseBadRequest));
+}).ConfigureApiBehaviorOptions(BadRequestBehavior.Parse);
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
