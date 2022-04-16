06847dea7b24e51d64ee255d8f2768353bd28eed
Stoycho Kostov <stoichokostov@gmail.com>
2022-04-15 20:10:35 -0500

    setting up pagination for actors controller

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -24,9 +24,11 @@ namespace MoviesAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<ActorDTO>>> Get()
+        public async Task<ActionResult<List<ActorDTO>>> Get([FromQuery] PaginationDTO paginationDTO)
         {
-            var actors = await context.Actors.ToListAsync();
+            var queryable = context.Actors.AsQueryable();
+            await HttpContext.InsertParametersPaginationInHeader(queryable);
+            var actors = await queryable.OrderBy(x => x.Name).Paginate(paginationDTO).ToListAsync();
             return mapper.Map<List<ActorDTO>>(actors);
         }
 
