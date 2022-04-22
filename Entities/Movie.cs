b20364e507064e867a9eba3ad73f6a32e5051238
Stoycho Kostov <stoichokostov@gmail.com>
2022-04-22 17:56:44 -0500

    movie creation backend

--- Entities/Movie.cs
+++ Entities/Movie.cs
@@ -15,5 +15,6 @@ namespace MoviesAPI.Entities
         public string Poster { get; set; }
         public List<MoviesGenres> MoviesGenres { get; set; }
         public List<MovieTheatersMovies> MovieTheatersMovies { get; set; }
+        public List<MoviesActors> MoviesActors { get; set; }
     }
 }
---
+++ Helpers/TypeBinder.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace MoviesAPI.Helpers
+{
+    public class TypeBinder<T> : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var propertyName = bindingContext.ModelName;
+            var value = bindingContext.ValueProvider.GetValue(propertyName);
+
+            if (value == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+            else
+            {
+                try
+                {
+                    var deserializedValue = JsonConvert.DeserializeObject<T>(value.FirstValue);
+                    bindingContext.Result = ModelBindingResult.Success(deserializedValue);
+                }
+                catch
+                {
+                    bindingContext.ModelState.TryAddModelError(propertyName,
+                        "The given value is not of the correct typ");
+                }
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
