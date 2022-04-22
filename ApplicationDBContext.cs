83000789ef51ff2534bd2d29ba3d69e5e90e6689
Stoycho Kostov <stoichokostov@gmail.com>
2022-04-22 16:42:19 -0500

    creating movie entities

--- ApplicationDBContext.cs
+++ ApplicationDBContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Data.SqlTypes;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using MoviesAPI.Entities;
 
@@ -10,8 +11,27 @@ namespace MoviesAPI
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MoviesActors>()
+                .HasKey(x => new {x.ActorId, x.MovieId});
+
+            modelBuilder.Entity<MoviesGenres>()
+                .HasKey(x => new {x.GenreId, x.MovieId});
+
+            modelBuilder.Entity<MovieTheatersMovies>()
+                .HasKey(x => new { x.MovieTheaterId, x.MovieId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Actor> Actors{ get; set; }
         public DbSet<MovieTheater> MovieTheaters { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MoviesActors> MoviesActors { get; set; }
+        public DbSet<MoviesGenres> MoviesGenres { get; set; }
+        public DbSet<MovieTheatersMovies> MovieTheatersMovies { get; set; }
+
     }
 }
