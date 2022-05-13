acf93d9372ed02c1c72817c4da3adba37ed5fd00
Stoycho Kostov <stoichokostov@gmail.com>
2022-05-12 23:40:52 -0500

    adding landing page dto/get method to retrieve movie data

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -42,6 +42,30 @@ namespace MoviesAPI.Controllers
             return dto;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<LandingPageDTO>> Get()
+        {
+            var top = 6;
+            var today = DateTime.Today;
+
+            var upcomingReleases = await context.Movies
+                .Where(x => x.ReleaseDate > today)
+                .OrderBy(x => x.ReleaseDate)
+                .Take(top)
+                .ToListAsync();
+
+            var inTheaters = await context.Movies
+                .Where(x => x.InTheaters)
+                .OrderBy(x => x.ReleaseDate)
+                .Take(top)
+                .ToListAsync();
+
+            var landingPageDTO = new LandingPageDTO();
+            landingPageDTO.UpcomingReleases = mapper.Map<List<MovieDTO>>(upcomingReleases);
+            landingPageDTO.InTheaters = mapper.Map<List<MovieDTO>>(inTheaters);
+            return landingPageDTO;
+        }
+
         [HttpGet("PostGet")]
         public async Task<ActionResult<MoviePostGetDTO>> PostGet()
         {
